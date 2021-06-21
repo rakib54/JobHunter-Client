bb9268563f0136f95b1665c2fd85128d930dd50f
Rakibur Rahman <rakibur5454@gmail.com>
2021-06-21 22:13:11 +0600

    spinner added

--- src/components/JobCard/Job.js
+++ src/components/JobCard/Job.js
@@ -6,11 +6,11 @@ const Job = () => {
     const [search, setSearch] = useState('')
 
     useEffect(() => {
-        fetch('https://secret-dawn-37725.herokuapp.com/jobdetails?search='+search)
-        .then(res => res.json())
-        .then(data => {
-            setJobs(data)
-        })
+        fetch('https://secret-dawn-37725.herokuapp.com/jobdetails?search=' + search)
+            .then(res => res.json())
+            .then(data => {
+                setJobs(data)
+            })
     }, [search])
 
     const handleSearch = (event) => {
@@ -20,19 +20,27 @@ const Job = () => {
     return (
         <>
             <div className="my-5">
-                <h1 style={{color:'rgb(26, 23, 23)'}} className="text-center">Find Your Job's</h1>
+                <h1 style={{ color: 'rgb(26, 23, 23)' }} className="text-center">Find Your Job's</h1>
                 <div className="col-md-3 col-3 mx-auto mt-3">
-                <input onChange={handleSearch} className="form-control" type="text" placeholder="search job" />
+                    <input onChange={handleSearch} className="form-control" type="text" placeholder="search job" />
                 </div>
             </div>
             <div className="container-fluid mb-5">
                 <div className="row">
                     <div className="col-10 mx-auto">
-                        <div className="row gy-5">
-                           {
-                               jobs.map((job) => <JobCard job={job} key={job._id} />)
-                           }
-                        </div>
+                        {
+                            jobs.length ?
+                                <div className="row gy-5">
+                                    {
+                                        jobs.map((job) => <JobCard job={job} key={job._id} />)
+                                    }
+                                </div> :
+                                <div className="text-center">
+                                    <div className="spinner-border" role="status">
+                                    <span className="visually-hidden">Loading...</span>
+                                </div>
+                                </div>
+                        }
                     </div>
                 </div>
             </div>
